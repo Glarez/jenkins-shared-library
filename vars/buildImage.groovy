2b3b505151a3fb4bc6a6fd7ab7929529e10cbe34
Glarez <germanlarez@gmail.com>
2021-07-25 06:38:43 -0400

    added dynamic branch name and app name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call () {
+def call (String imageName) { // We use imageName for not to hardcode app name and version on the shared library
     echo "========Building the Docker image========"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t glarez/java-maven-app:1.0 .'
+        sh "docker build -t $imageName ."
         echo "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push glarez/java-maven-app:1.0'
+        sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call () {
-    echo "========executing Maven Build========"
+    echo "========executing Maven Build for $BRANCH_NAME========"
     sh "mvn package"
 }
