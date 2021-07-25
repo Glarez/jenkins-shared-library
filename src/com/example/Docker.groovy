6795a7838ddfb299beca973440f5a5b910a9f451
Glarez <germanlarez@gmail.com>
2021-07-25 07:43:57 -0400

    extracted logic into separate classes II Now it's personal!!

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -4,15 +4,18 @@ package com.example
 
 class   Docker  implements Serializable {
 
-        def script
-        Docker(script) {
-            this.script = script
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "========Building the Docker image========"
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.echo "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh "docker push $imageName"
         }
-        def buildDockerImage(String imageName) {
-            script.echo "========Building the Docker image========"
-            script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                script.sh "docker build -t $imageName ."
-                script.echo "echo $script.PASS | docker login -u $script.USER --password-stdin"
-                script.sh "docker push $imageName"
-            }
-        }
+    }
+}
