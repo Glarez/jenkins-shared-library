4e6884b7697e3d8407d6bf43edaf6eb92314a307
Glarez <germanlarez@gmail.com>
2021-07-25 07:36:23 -0400

    extracted logic into separate classes

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+package com.example
+
+class   Docker  implements Serializable {
+
+        def script
+        Docker(script) {
+            this.script = script
+        }
+    def buildDockerImage(String imageName) {
+        script.echo "========Building the Docker image========"
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.echo "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh "docker push $imageName"
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
+import com.example.Docker
 def call (String imageName) { // We use imageName for not to hardcode app name and version on the shared library
-    echo "========Building the Docker image========"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $imageName ."
-        echo "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push $imageName"
+        return new Docker(this).buildDockerImage(imageName)
     }
 }
